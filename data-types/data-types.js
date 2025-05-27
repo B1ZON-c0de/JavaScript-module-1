4d5feb93d7e492005b7e10f8717f73168fe61e6e
B1ZON-c0de <roman.andreicko@icloud.com>
2025-05-27 23:28:35 +0700

    Add files via upload

---
+++ data-types/data-types.js
@@ -0,0 +1,21 @@
+const str = 'Это строка!';
+const num = 10;
+const bigNumber = 20n;
+const bool = true;
+const nothing = null;
+let undf;
+const obj = {};
+const id = Symbol();
+// Есть ещё и такое
+const alrt = alert;
+
+console.log('str: ', str, typeof str);
+console.log('num:  ', num, typeof num);
+console.log('bigNumber: ', bigNumber, typeof bigNumber);
+console.log('bool: ', bool, typeof bool);
+console.log('nothing: ', nothing, typeof nothing, 'хахаха это объект');
+console.log('undf: ', undf, typeof undf);
+console.log('obj: ', obj, typeof obj);
+console.log('id: ', id, typeof id);
+//
+console.log('alrt: ', alrt, typeof alrt);
---
+++ index/index.js
@@ -0,0 +1,11 @@
+let firstName = 'Vladimir';
+let age = 25;
+let isProgrammer = false;
+
+age = '25';
+firstName = 101;
+isProgrammer = null;
+
+alert(firstName);
+alert(age);
+alert(isProgrammer);
